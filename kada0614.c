8aaa6375fe92080dfc72667a64b7f567363c3f15
00kenno <162081854+00kenno@users.noreply.github.com>
2024-06-20 15:23:44 +0900

    Update kada0614.c

--- kada0614.c
+++ kada0614.c
@@ -2,10 +2,10 @@
 void main(){
   int i, n;
   float un[21], 
-        unpl[21], 
+        unp1[21], 
         u, //ディスク表面の回転速度
         rho, tau, nu, //定数
-        c, //= d^2u/dy^2
+        dt, 
         dy
         ;
 
@@ -13,9 +13,11 @@ void main(){
   nu = 0.00001;
   rho = 1.2;
   dy = 1 / 20 / 1000;//mm->m
+
+  printf("u=%f\nnu=%f\nrho=%f\ndy=%f\n",u,nu,rho,dy);
   
-  printf("Input c \n");
-  scanf("%f",&c);
+  printf("Input dt \n");
+  scanf("%f",&dt);
   printf("\n");
   
   un[20] = u;
@@ -27,14 +29,14 @@ void main(){
   for(n = 0; n <=20000; n++){//n = 20000*dt　まで繰り返す
 
     for(i = 1; i <= 19; i++){//unpl[1~19]を計算
-      unpl[i] = un[i] + c * nu * (un[i+1] - 2.0*un[i] + un[i-1]);
+      unp1[i] = un[i] + nu * dt * (un[i+1] - 2.0*un[i] + un[i-1]) / dy / dy;
     }
 
-    unpl[0] = 0.0;
-    unpl[20] = u;
+    unp1[0] = 0.0;
+    unp1[20] = u;
 
     for(i = 0; i <= 20; i++){
-      un[i] = unpl[i];
+      un[i] = unp1[i];
     }
     
   }
