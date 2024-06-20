9e6223dbfd115d58ad7b03f78db904a03419328f
00kenno <162081854+00kenno@users.noreply.github.com>
2024-06-20 11:55:50 +0900

    Update kada0614.c

--- kada0614.c
+++ kada0614.c
@@ -1,39 +1,51 @@
 #include<stdio.h>
 void main(){
   int i, n;
-  float tn[21], tnpl[21], c, u, rho, tau, nu, dt, u_tt;
+  float un[21], 
+        unpl[21], 
+        u, //ディスク表面の回転速度
+        rho, tau, nu, //定数
+        c, //= d^2u/dy^2
+        dy
+        ;
 
   u = 700 * 0.02;
   nu = 0.00001;
   rho = 1.2;
-
-  u_tt =
-
+  dy = 1 / 20;
+  
   printf("Input c \n");
   scanf("%f",&c);
-  tn[20] = 2000.0;
+  printf("\n");
+  
+  un[20] = u;
 
   for(i = 0; i <= 19; i++){
-    tn[i] = 300.0;
+    tn[i] = 0.0;
   }
 
-  for(n = 0; n <=2000; n++){
+  for(n = 0; n <=2000; n++){//n = 2000*dt　まで繰り返す
 
-    for(i = 1; i <= 19; i++){
-      tnpl[i] = tn[i] + c * (tn[i+1] - 2.0*tn[i] + tn[i-1]);
+    for(i = 1; i <= 19; i++){//unpl[1~19]を計算
+      unpl[i] = un[i] + c * nu * (un[i+1] - 2.0*un[i] + un[i-1]);
     }
 
-    tnpl[0] = 300.0;
-    tnpl[20] = 2000.0;
+    unpl[0] = 0.0;
+    unpl[20] = u;
 
     for(i = 0; i <= 20; i++){
-      tn[i] = tnpl[i];
+      un[i] = unpl[i];
     }
-
+    
   }
 
-  for(i = 0; i <= 20; i++){
-    printf("i = %d  T = %f \n", i, tn[i]);
+  for(i = 0; i <= 20; i++){//un[0~20]を表示
+    printf("i = %d  u = %f \n", i, tn[i]);
   }
+
+  tau = rho * nu * (un[i+1] - un[i]) / dy;
+  printf("****************\n");
+  printf("tau = %f\n", tau);
+  printf("****************\n");
 
 }
